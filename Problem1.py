8d23581486942547493356e99aa3d104dba26fce
Nihad1999 <47653259+Nihad1999@users.noreply.github.com>
2019-02-24 09:09:19 +0300

    Create Problem1.py

---
+++ Problem1.py
@@ -0,0 +1,8 @@
+print("Matrisin elementlerini daxil edin. Aralarinda bosluq qoyun. Vergule ehtiyac yoxdu")
+matrix = [int(x) for x in input().split()]
+
+enboyuk = max(matrix)
+
+print(matrix)
+
+print(enboyuk)
